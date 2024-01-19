9bd7e11c9ca6adcfacdbbd4d75ae0630450e7ea9
zhiayang <gpg@tsumaran.ai>
2024-01-19 01:44:41 -0500

    stop bootstrap.sh editing ~/.bash_profile; close #1

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -14,10 +14,9 @@ _n=$(tput sgr0)
 cat <<EOF
 ${_b}1.${_n} Download bootstrap archive files from github
 ${_b}2.${_n} Unpack the archive at ${_g}/opt/pacman/${_n}
-${_b}3.${_n} Edit ${_g}~/.bash_profile${_n} to add /opt/pacman/usr/bin to \$PATH
-${_b}4.${_n} Add ${_g}/opt/pacman/usr/bin/bash${_n} to ${_r}/etc/shells${_n}
-${_b}5.${_n} Change your login shell to ${_g}/opt/pacman/usr/bin/bash${_n}
-${_b}6.${_n} Initialise Pacman
+${_b}3.${_n} Add ${_g}/opt/pacman/usr/bin/bash${_n} to ${_r}/etc/shells${_n}
+${_b}4.${_n} Change your login shell to ${_g}/opt/pacman/usr/bin/bash${_n}
+${_b}5.${_n} Initialise Pacman
    ${_b}a)${_n} Set up Pacman keyrings
    ${_b}b)${_n} Synchronise packages (via ${_g}pacman -Syu${_n})
 EOF
@@ -40,22 +39,6 @@ sudo mkdir /opt/pacman
 
 msg "Unpacking archive"
 sudo tar xpf bootstrap.tar.xz -C /
-
-msg "Setting up \$PATH"
-if [ -e $HOME/.bash_profile ]; then
-	printf '\x1b[34;1m  ->\x1b[0m \x1b[1mFound existing `.bash_profile`, backing up to `.bash_profile.old`\x1b[0m\n'
-	mv $HOME/.bash_profile $HOME/.bash_profile.old
-fi
-
-cat <<'EOF' > $HOME/.bash_profile
-export PATH=/opt/pacman/usr/bin:$PATH
-export CPATH=/opt/pacman/usr/include
-export LIBRARY_PATH=/opt/pacman/usr/lib
-export MANPATH=/opt/pacman/usr/share/man:$MANPATH
-
-export XDG_CONFIG_DIRS=/opt/pacman/etc/xdg
-export XDG_DATA_DIRS=/opt/pacman/usr/share
-EOF
 
 msg "Adding /opt/pacman/usr/bin/bash to /etc/shells"
 echo "/opt/pacman/usr/bin/bash" | sudo tee -a /etc/shells
